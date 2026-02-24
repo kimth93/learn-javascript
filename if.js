a49be45f4c912b4d62c5dc15b89c6054c7cda18f
kimth93 <sasa1018@naver.com>
2026-02-24 14:21:30 +0900

    learn javascript

---
+++ if.js
@@ -0,0 +1,22 @@
+console.log("if");
+const score = 85;
+
+// if (score >= 60) {
+//   console.log("합격");
+// } else {
+//   console.log("불합격");
+// }
+
+//else if
+let grade = "A";
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+} else if (score >= 70) {
+  grade = "C";
+} else {
+  grade = "D";
+}
+
+console.log(`당신의 학점은 ${grade}입니다.`);
